0e3fea024ee7a70ad7bc11b42a6cfb13f072b748
Mike Herwig <Mike.Herwig@telekom.de>
2024-06-26 15:22:42 +0200

    fix: added missing package

---
+++ internal/log/log.go
@@ -0,0 +1,20 @@
+package log
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+func SetLogLevel(level string) {
+	logLevel, err := zerolog.ParseLevel(level)
+	if err != nil {
+		logLevel = zerolog.InfoLevel
+		log.Info().Msgf("Invalid log level %s. Info log level is used", logLevel)
+	}
+
+	log.Logger = zerolog.New(os.Stdout).Level(logLevel).With().Timestamp().Logger()
+	if logLevel == zerolog.DebugLevel {
+		log.Logger = log.Logger.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+	}
+}
