9ac74fb3e0209147bf2d4f4c30648a690156cf68
Julian Spierefka <Julian.Spierefka@telekom.de>
2024-07-10 16:53:45 +0200

    add mock cache for healthCheckCache and republishingCache

---
+++ internal/test/health_check_mock.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"context"
+	"github.com/hazelcast/hazelcast-go-client/types"
+	"github.com/stretchr/testify/mock"
+	"time"
+)
+
+type HealthCheckMockMap struct {
+	mock.Mock
+}
+
+func (h *HealthCheckMockMap) Get(ctx context.Context, key interface{}) (interface{}, error) {
+	args := h.Called(ctx, key)
+	return args.Get(0), args.Error(1)
+}
+
+func (h *HealthCheckMockMap) Set(ctx context.Context, key interface{}, value interface{}) error {
+	args := h.Called(ctx, key, value)
+	return args.Error(0)
+}
+
+func (h *HealthCheckMockMap) TryLockWithTimeout(ctx context.Context, key interface{}, timeout time.Duration) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HealthCheckMockMap) GetEntrySet(ctx context.Context) ([]types.Entry, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HealthCheckMockMap) NewLockContext(ctx context.Context) context.Context {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HealthCheckMockMap) Delete(ctx context.Context, key interface{}) error {
+	args := h.Called(ctx, key)
+	return args.Error(0)
+}
+
+func (h *HealthCheckMockMap) Unlock(ctx context.Context, key interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HealthCheckMockMap) IsLocked(ctx context.Context, key interface{}) (bool, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HealthCheckMockMap) ForceUnlock(ctx context.Context, key interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ internal/test/republishing_mock.go
@@ -0,0 +1,57 @@
+package test
+
+import (
+	"context"
+	"github.com/hazelcast/hazelcast-go-client/types"
+	"github.com/stretchr/testify/mock"
+	"time"
+)
+
+type RepublishingMockMap struct {
+	mock.Mock
+}
+
+func (r *RepublishingMockMap) Get(ctx context.Context, key interface{}) (interface{}, error) {
+	args := r.Called(ctx, key)
+	return args.Get(0), args.Error(1)
+}
+
+func (r *RepublishingMockMap) Set(ctx context.Context, key interface{}, value interface{}) error {
+	args := r.Called(ctx, key, value)
+	return args.Error(0)
+}
+
+func (r *RepublishingMockMap) TryLockWithTimeout(ctx context.Context, key interface{}, timeout time.Duration) (bool, error) {
+	args := r.Called(ctx, key, timeout)
+	return args.Bool(0), args.Error(1)
+}
+
+func (r *RepublishingMockMap) GetEntrySet(ctx context.Context) ([]types.Entry, error) {
+	args := r.Called(ctx)
+	return args.Get(0).([]types.Entry), args.Error(1)
+}
+
+func (r *RepublishingMockMap) NewLockContext(ctx context.Context) context.Context {
+	args := r.Called(ctx)
+	return args.Get(0).(context.Context)
+}
+
+func (r *RepublishingMockMap) Delete(ctx context.Context, key interface{}) error {
+	args := r.Called(ctx, key)
+	return args.Error(0)
+}
+
+func (r *RepublishingMockMap) IsLocked(ctx context.Context, key interface{}) (bool, error) {
+	args := r.Called(ctx, key)
+	return args.Bool(0), args.Error(1)
+}
+
+func (r *RepublishingMockMap) Unlock(ctx context.Context, key interface{}) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *RepublishingMockMap) ForceUnlock(ctx context.Context, key interface{}) error {
+	args := r.Called(ctx, key)
+	return args.Error(0)
+}
