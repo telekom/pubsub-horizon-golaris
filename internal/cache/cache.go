ebb9fd4160e83fb6c69c82ac05037f80b96d684f
Julian Spierefka <Julian.Spierefka@telekom.de>
2024-07-10 16:55:18 +0200

    change hazelcastMap to hazelcastMapInterface to get the possibility of mocking the caches

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -8,19 +8,37 @@ import (
 	"context"
 	"fmt"
 	"github.com/hazelcast/hazelcast-go-client"
+	"github.com/hazelcast/hazelcast-go-client/types"
 	"github.com/rs/zerolog/log"
 	c "github.com/telekom/pubsub-horizon-go/cache"
 	"github.com/telekom/pubsub-horizon-go/message"
 	"github.com/telekom/pubsub-horizon-go/resource"
 	"github.com/telekom/pubsub-horizon-go/util"
 	"golaris/internal/config"
+	"sync"
+	"time"
 )
+
+type HazelcastMapInterface interface {
+	Get(ctx context.Context, key interface{}) (interface{}, error)
+	Set(ctx context.Context, key interface{}, value interface{}) error
+	TryLockWithTimeout(ctx context.Context, key interface{}, timeout time.Duration) (bool, error)
+	GetEntrySet(ctx context.Context) ([]types.Entry, error)
+	NewLockContext(ctx context.Context) context.Context
+	Delete(ctx context.Context, key interface{}) error
+	Unlock(ctx context.Context, key interface{}) error
+	IsLocked(ctx context.Context, key interface{}) (bool, error)
+	ForceUnlock(ctx context.Context, key interface{}) error
+}
 
 var Subscriptions c.HazelcastBasedCache[resource.SubscriptionResource]
 var CircuitBreakers c.HazelcastBasedCache[message.CircuitBreakerMessage]
-var HealthChecks *hazelcast.Map
-var RepublishingCache *hazelcast.Map
+var HealthChecks HazelcastMapInterface
+var RepublishingCache HazelcastMapInterface
 var hazelcastClient *hazelcast.Client
+
+var SubscriptionCancelMap = make(map[string]bool)
+var CancelMapMutex sync.Mutex
 
 func Initialize() {
 	c := createNewHazelcastConfig()
