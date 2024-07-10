6b74a7ae3e286abb3cf9af6a19395c08e86db642
Heiner Egbers <heiner.egbers@telekom.de>
2024-07-10 15:51:35 +0200

    feat(cb-api): added function getCircuitBreakerMessageById for retrieving subscription by ID

--- internal/api/api.go
+++ internal/api/api.go
@@ -5,11 +5,15 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/healthcheck"
 	"github.com/gofiber/fiber/v2/middleware/pprof"
 	"github.com/rs/zerolog/log"
+
+	"pubsub-horizon-golaris/internal/cache"
+	"pubsub-horizon-golaris/internal/config"
 	"pubsub-horizon-golaris/internal/metrics"
 	"pubsub-horizon-golaris/internal/tracing"
 )
@@ -26,14 +30,44 @@ func init() {
 	app.Use(pprof.New())
 
 	app.Get("/metrics", metrics.NewPrometheusMiddleware())
-
+	// setup routes
 	v1 := app.Group("/api/v1")
 
-	v1.Get("/circuit-breakers/:subscriptionId", getCircuitBreakerMessage)
+	v1.Get("/circuit-breakers/:subscriptionId", getCircuitBreakerMessageById)
+
+	v1.Get("/circuit-breakers", getAllCircuitBreakerMessages)
 }
 
-func getCircuitBreakerMessage(c *fiber.Ctx) error {
-	return c.SendString("Hello, World!")
+func getAllCircuitBreakerMessages(ctx *fiber.Ctx) error {
+
+	//return hello world
+	return ctx.SendString("Hello, World!")
+}
+
+func getCircuitBreakerMessageById(ctx *fiber.Ctx) error {
+	// Get the subscriptionId from the request parameters
+	subscriptionId := ctx.Params("subscriptionId")
+
+	// Read from the circuit breaker cache
+	cbMessage, err := cache.CircuitBreakerCache.Get(config.Current.Hazelcast.Caches.CircuitBreakerCache, subscriptionId)
+
+	if err != nil {
+		log.Error().Err(err).Msgf("Error while getting CircuitBreaker message for subscription %s", subscriptionId)
+		return ctx.Status(fiber.StatusInternalServerError).SendString("Error retrieving circuit breaker message")
+	}
+	//add if cbMessage is nil, then return not found status code
+	if cbMessage == nil {
+		return ctx.Status(fiber.StatusNotFound).SendString("Circuit breaker message not found for subscription-id " + subscriptionId)
+	}
+	// Convert the circuit breaker message to JSON
+	cbMessageJSON, err := json.Marshal(cbMessage)
+	if err != nil {
+		log.Error().Err(err).Msg("Error converting circuit breaker message to JSON")
+		return ctx.Status(fiber.StatusInternalServerError).SendString("Error converting circuit breaker message to JSON")
+	}
+
+	// Send the circuit breaker message as the response
+	return ctx.Status(fiber.StatusOK).Send(cbMessageJSON)
 }
 
 func Listen(port int) {
