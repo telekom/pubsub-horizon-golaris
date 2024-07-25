0b5da2501564665f1e336ebec3d9faa0d207d02b
Julian Spierefka <Julian.Spierefka@telekom.de>
2024-07-25 13:49:57 +0200

    remove SetCancelStatus(subscriptionId, false)

--- internal/republish/republish.go
+++ internal/republish/republish.go
@@ -102,7 +102,7 @@ func RepublishPendingEvents(subscription *resource.SubscriptionResource, republi
 	batchSize := config.Current.Republishing.BatchSize
 	page := int64(0)
 
-	cache.SetCancelStatus(subscriptionId, false)
+	// SetCancelStatus(subscriptionId, false)
 
 	throttler = createThrottler(subscription.Spec.Subscription.RedeliveriesPerSecond)
 	throttlingEnabled := !(subscription.Spec.Subscription.DeliveryType == "sse" || subscription.Spec.Subscription.DeliveryType == "server_sent_event")
